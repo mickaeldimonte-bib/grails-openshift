6723f56dca7bc5d0f0ab6e2c779a6591a3ed2538
fx11269 <FX11269@adgroup.michelin.com>
2016-10-03 10:01:22 +0200

    Modify Datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,4 +1,12 @@
+/*dataSource {
+    pooled = true
+    jmxExport = true
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""
+}*/
 dataSource {
+    pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
     dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
@@ -13,34 +21,35 @@ hibernate {
 
 // environment specific settings
 environments {
-    development {
+    /*development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:mysql://localhost/grails_db?useUnicode=yes&characterEncoding=UTF-8"
-            username = "root"
-            password = ""
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }*/
+  development {
+        dataSource {
+          dbCreate = "update"
+          url = "jdbc:mysql://eu-cdbr-azure-west-d.cloudapp.net:3306/grails_db"
+          username = "bf90c22556e473"
+          password = "9c68c271"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://eu-cdbr-azure-west-d.cloudapp.net:3306/grails_db"
+            username = "sa"
+            password = ""
         }
     }
     production {
         dataSource {
           dbCreate = "update"
-          driverClassName = 'com.mysql.jdbc.Driver'
-          dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-
-          String host = System.getenv('APPSETTING_MYSQL_DB_HOST')
-          String port = System.getenv('APPSETTING_MYSQL_DB_PORT')
-          String dbName = System.getenv('APPSETTING_MYSQL_DB_NAME')
-
           url = "jdbc:mysql://eu-cdbr-azure-west-d.cloudapp.net:3306/grails_db"
-
-          username = bf90c22556e473//System.getenv('APPSETTING_MYSQL_DB_USERNAME')
-          password = 9c68c271//System.getenv('APPSETTING_MYSQL_DB_PASSWORD')
-       }
+          username = "bf90c22556e473"
+          password = "9c68c271"
+        }
     }
 }
+
